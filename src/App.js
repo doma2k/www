18a9c61a87b2c29449a0134efca58cda14d33215
doma2k <79820904+doma2k@users.noreply.github.com>
2022-12-06 22:59:19 +0000

    2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Background from './background.png'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="App-header" style={{
+        background: `url(${Background}) `,
+      }}>
+        <img src={logo} className="App-logo" alt="logo" style={{
+          backgroundColor: 'white',
+        }} />
+        <p className="Text" style={{
+          backgroundColor: 'white',
+        }}>
+          Web3, to be continued....
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
